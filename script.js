5370b43e3838ae05d9c9d8a3f525e6fa459ce19c
Zuhriddin <Zuha4009@gmail.com>
2022-10-26 21:32:55 +0500

    js4

--- script.js
+++ script.js
@@ -68,84 +68,83 @@
 
 // HOMEWORK1__________________________________________________________________________________________
 
-// var number = +prompt('Yoshingizni kiriting!')
+var number = +prompt('Yoshingizni kiriting!')
 
-// if (number < 18 && !isNaN(number)) {
-//     alert(number + ' Siz hali yoshsiz. Oqishingiz kerak! ')
-// } else if (number == 18 && !isNaN(number)) {
-//     alert(number + ' Siz hali yoshsiz. Oqishingiz kerak! ')
-// } else {
-//     if (number < 50 && !isNaN(number)) {
-//         alert(number + ' Siz ishlashingiz kerak! ')
-//     } else if (number == 18 && !isNaN(number)) {
-//         alert(number + ' Siz ishlashingiz kerak ')
-//     } else {
-//         if (number < 59 && !isNaN(number)) {
-//             alert(number + ' Siz yaqinda nafaqaga ketasiz! ')
-//         } else if (number == 50 && !isNaN(number)) {
-//             alert(number + ' Siz yaqinda nafaqaga ketasiz ')
-//         } else {
-//             if (number < 150 && !isNaN(number)) {
-//                 alert(number + ' Siz nafaqadasiz! ')
-//             } else if (number == 59 && !isNaN(number)) {
-//                 alert(number + ' Siz nafaqadasiz ')
-//             } else {
-//                 if (isNaN(number)) {
-//                     alert('Siz umuman son kiritmadingiz')
-//                 } else if (isNaN(number)) {
-//                     alert('Son kiriting')
-//                 } else (
-//                     alert(number + ' Nimadir notogri ketti ')
-//                 )
+if (number < 18 && !isNaN(number)) {
+    alert(number + ' Siz hali yoshsiz. Oqishingiz kerak! ')
+} else if (number == 18 && !isNaN(number)) {
+    alert(number + ' Siz hali yoshsiz. Oqishingiz kerak! ')
+} else {
+    if (number < 50 && !isNaN(number)) {
+        alert(number + ' Siz ishlashingiz kerak! ')
+    } else if (number == 18 && !isNaN(number)) {
+        alert(number + ' Siz ishlashingiz kerak ')
+    } else {
+        if (number < 59 && !isNaN(number)) {
+            alert(number + ' Siz yaqinda nafaqaga ketasiz! ')
+        } else if (number == 50 && !isNaN(number)) {
+            alert(number + ' Siz yaqinda nafaqaga ketasiz ')
+        } else {
+            if (number < 150 && !isNaN(number)) {
+                alert(number + ' Siz nafaqadasiz! ')
+            } else if (number == 59 && !isNaN(number)) {
+                alert(number + ' Siz nafaqadasiz ')
+            } else {
+                if (isNaN(number)) {
+                    alert('Siz umuman son kiritmadingiz')
+                } else if (isNaN(number)) {
+                    alert('Son kiriting')
+                } else (
+                    alert(number + ' Nimadir notogri ketti ')
+                )
 
 
-//             }
-//         }
-//     }
-// }
-
+            }
+        }
+    }
+}
 
 // HOMEWORK2__________________________________________________________________________________________
 
-/* var number = +prompt('Soat nechi bo\'ldi')
+var number = +prompt('Soatni 0 dan 24 gacha kiriting')
 
 if (number >= 7 && number <= 11) {
-    alert(" Tong soat " + number)
+    alert(" Toshkent vaqti bilan soat tongi " + number + ":00")
 } else if (number == 12 && !isNaN(number)) {
-    alert(" Tong soat 12 ")
+    alert(" Toshkent vaqti bilan soat tongi 12:00 ")
 } else {
     if (number == 13 && !isNaN(number)) {
-        alert(" Kun soat 1 ")
+        alert(" Toshkent vaqti bilan soat kunduzgi 1:00 ")
     } else if (number == 14 && !isNaN(number)) {
-        alert(" Kun soat 2 ")
+        alert(" Toshkent vaqti bilan soat kunduzgi 2:00 ")
     } else {
         if (number == 15 && !isNaN(number)) {
-            alert(" Kun soat 3 ")
+            alert(" Toshkent vaqti bilan soat kunduzgi 3:00 ")
         } else if (number == 16 && !isNaN(number)) {
-            alert(" Kun soat 4 ")
+            alert(" Toshkent vaqti bilan soat kunduzgi 4:00 ")
         } else {
             if (number == 17 && !isNaN(number)) {
-                alert(" Kun soat 5 ")
+                alert(" Toshkent vaqti bilan soat kunduzgi 5:00 ")
             } else if (number == 18 && !isNaN(number)) {
-                alert(" Kech soat 6 ")
+                alert(" Toshkent vaqti bilan soat kunduzgi 6:00 ")
             } else {
                 if (number == 19 && !isNaN(number)) {
-                    alert(" Kech soat 7 ")
+                    alert(" Toshkent vaqti bilan soat kechgi 7:00 ")
                 } else if (number == 20 && !isNaN(number)) {
-                    alert(" Kech soat 8 ")
+                    alert(" Toshkent vaqti bilan soat kechgi 8:00 ")
                 } else {
                     if (number == 21 && !isNaN(number)) {
-                        alert(" Kech soat 9 ")
+                        alert(" Toshkent vaqti bilan soat kechgi 9:00 ")
                     } else if (number == 22 && !isNaN(number)) {
-                        alert(" Tun soat 10 ")
+                        alert(" Toshkent vaqti bilan soat tungi 10:00 ")
                     } else {
                         if (number == 23 && !isNaN(number)) {
-                            alert(" Tun soat 11 ")
+                            alert(" Toshkent vaqti bilan soat tungi 11:00 ")
                         } else if (number == 24 && !isNaN(number)) {
-                            alert(" Tun soat 12 ")
+                            alert(" Toshkent vaqti bilan soat tungi 12:00 ")
                         } else {
                             if (number >= 1 && number <= 6) {
-                                alert(" Tun soat " + number)
+                                alert(" Toshkent vaqti bilan soat tungi " + number + ":00")
                             } else if (isNaN(number)) {
                                 alert('Soatni sonda kiriting')
                             } else {
@@ -158,14 +157,13 @@ if (number >= 7 && number <= 11) {
             }
         }
     }
-} */
-
+}
 
 // HOMEWORK3__________________________________________________________________________________________
 
-var number1 = +prompt('Son kiriting')
-var number2 = +prompt('Son kiriting')
-var number3 = +prompt('Son kiriting')
+var number1 = +prompt('Birinch sonni kiriting')
+var number2 = +prompt('Ikkinchi sonni kiriting')
+var number3 = +prompt('Uchinchi sonni kiriting')
 
 if((number1 > number2 && number1 < number3) || (number1 < number2 && number1 > number3)){
     alert(number1)
@@ -173,6 +171,8 @@ if((number1 > number2 && number1 < number3) || (number1 < number2 && number1 > n
     alert(number2)
 }else if((number3 > number2 && number3 < number1) || (number3 < number2 && number3 > number1)){
     alert(number3)
+}else if((number3 == number2 && number3 == number1) || (number3 == number2 && number3 == number1)){
+    alert("Kiritilgan sonlar teng")
 }else{
     alert(' Nimadir notogri ketti ')
 }
